53a42e74699347a0ce8cc35d92a4afec6b217cbb
xdunifon <xdunifon@gmail.com>
2026-02-21 18:30:57 -0500

    Reimplement the prompt service

--- LooseEndsApi/LooseEnds.Api/Resources/Prompts/PromptService.cs
+++ LooseEndsApi/LooseEnds.Api/Resources/Prompts/PromptService.cs
@@ -5,69 +5,57 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LooseEnds.Api.Resources.Prompts;
 
-public class PromptService : BaseService
+public interface IPromptService
 {
-    public PromptService(GameContext context) : base(context) { }
+    Task<Prompt?> GetRandomPromptAsync(int[]? excludedPromptIds = null);
+}
 
-    public async Task<List<RoundPrompt>> CreateRoundPrompts(GameSession session, Round round)
+public class PromptService(GameContext context) : BaseService(context), IPromptService
+{
+    public async Task<Prompt?> GetRandomPromptAsync(int[]? excludedPromptIds)
     {
-        var assignedPlayers = new List<Player>();
-        int promptCount = (session.Players.Count() + 1) / 2; // Handles odd/even automatically
-        var roundPrompts = new List<RoundPrompt>(promptCount);
+        var query = _context.Prompts.Where(p => p.Active && !excludedPromptIds.Contains(p.Id));
+        
+        // Return null if the exclusion list excludes all prompts
+        var count = await query.CountAsync();
+        if (count == 0) return null;
+        
+        // Get valid prompt IDs
+        var promptIds = await query.Select(p => p.Id).ToArrayAsync();
 
-        for (int i = 0; i < promptCount; i++)
-        {
-            var promptPlayers = new List<Player>();
-            for (int j = 0; j < 2; j++)
-            {
-                Player? player = session.GetRandomPlayer(assignedPlayers);
-                if (player != null)
-                {
-                    assignedPlayers.Add(player); // add to assigned list so they aren't assigned a second time
-                    promptPlayers.Add(player);
-                } else
-                {
-                    // This may change but the current idea is to add a bot player that the DB is seeded with
-                    promptPlayers.Add(await _context.Players.FindAsync(0));
-                }
-            }
-            var prompt = await CreateRoundPrompt(round, promptPlayers);
-            roundPrompts.Add(prompt);
-        }
-
-        return roundPrompts;
-    }
-
-    public async Task<RoundPrompt> CreateRoundPrompt(Round round, List<Player> promptPlayers)
-    {
-        var prompt = await GetRandomPrompt();
-
-        var roundPrompt = new RoundPrompt(round, prompt.Content, 30);
-
-        roundPrompt.PlayerResponses = promptPlayers.Select(player => new PlayerResponse(player, roundPrompt)).ToList();
-
-        return roundPrompt;
-    }
-
-
-    public async Task<Prompt> GetRandomPrompt()
-    {
+        // Randomly select
         var rng = new Random();
-        int ranIndex = rng.Next(0, await _context.Prompts.CountAsync());
-        List<int> promptIds = await _context.Prompts.Select(prompt => prompt.Id).ToListAsync();
-        return await _context.Prompts.FindAsync(promptIds[ranIndex]);
+        var selectedId = promptIds.ElementAt(rng.Next(0, promptIds.Length));
+
+        return await _context.Prompts.FindAsync(selectedId);
     }
 
-    public async Task<Player?> GetRandomPlayer(int sessionId, List<Player> listToExclude)
-    {
-        var rng = new Random();
-        List<Player> players = await _context.Players.Where(player => player.SessionId == sessionId && !listToExclude.Contains(player)).ToListAsync();
-        if (players.Count == 0)
-        {
-            return null;
-        }
+    //public async Task<List<RoundPrompt>> CreateRoundPrompts(GameSession session, Round round)
+    //{
+    //    var assignedPlayers = new List<Player>();
+    //    int promptCount = (session.Players.Count() + 1) / 2; // Handles odd/even automatically
+    //    var roundPrompts = new List<RoundPrompt>(promptCount);
 
-        int ranIndex = rng.Next(0, players.Count);
-        return players[ranIndex];
-    }
+    //    for (int i = 0; i < promptCount; i++)
+    //    {
+    //        var promptPlayers = new List<Player>();
+    //        for (int j = 0; j < 2; j++)
+    //        {
+    //            Player? player = session.GetRandomPlayer(assignedPlayers);
+    //            if (player != null)
+    //            {
+    //                assignedPlayers.Add(player); // add to assigned list so they aren't assigned a second time
+    //                promptPlayers.Add(player);
+    //            } else
+    //            {
+    //                // This may change but the current idea is to add a bot player that the DB is seeded with
+    //                promptPlayers.Add(await _context.Players.FindAsync(0));
+    //            }
+    //        }
+    //        var prompt = await CreateRoundPrompt(round, promptPlayers);
+    //        roundPrompts.Add(prompt);
+    //    }
+
+    //    return roundPrompts;
+    //}
 }
