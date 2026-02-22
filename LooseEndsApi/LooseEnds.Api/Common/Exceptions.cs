7cb70faf3319de8ae10e7462cb82e84ced0208a5
xdunifon <xdunifon@gmail.com>
2026-02-22 14:36:49 -0500

    Add exception helpers

--- LooseEndsApi/LooseEnds.Api/Common/Exceptions.cs
+++ LooseEndsApi/LooseEnds.Api/Common/Exceptions.cs
@@ -1,3 +1,12 @@
 ﻿namespace LooseEnds.Api.Common;
 
+public static class GameExceptions
+{
+    public static NotFoundException GameNotFound(string gameCode) => new NotFoundException($"Couldn't find game with code {gameCode}");
+    public static GameStartedException AlreadyStarted(string gameCode) => new GameStartedException($"Game with code {gameCode} has already started");
+    public static ThreePlayersRequiredException ThreeRequired() => new ThreePlayersRequiredException($"Three players are required before starting the game");
+}
+
 public class NotFoundException(string message) : Exception(message) { }
+public class GameStartedException(string message) : Exception(message) { }
+public class ThreePlayersRequiredException(string message): Exception(message) { }
