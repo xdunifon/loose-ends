7919778aaa00b80e720810d9fa1c27038984748b
xdunifon <xdunifon@gmail.com>
2026-02-22 20:53:54 -0500

    Add invalid answer & vote exceptions

--- LooseEndsApi/LooseEnds.Api/Common/Exceptions.cs
+++ LooseEndsApi/LooseEnds.Api/Common/Exceptions.cs
@@ -5,8 +5,12 @@ public static class GameExceptions
     public static NotFoundException GameNotFound(string gameCode) => new NotFoundException($"Couldn't find game with code {gameCode}");
     public static GameStartedException AlreadyStarted(string gameCode) => new GameStartedException($"Game with code {gameCode} has already started");
     public static ThreePlayersRequiredException ThreeRequired() => new ThreePlayersRequiredException($"Three players are required before starting the game");
+    public static InvalidAnswer InvalidAnswer() => new InvalidAnswer($"You cannot answer this prompt");
+    public static InvalidVote InvalidVote() => new InvalidVote($"You cannot vote on this response");
 }
 
 public class NotFoundException(string message) : Exception(message) { }
 public class GameStartedException(string message) : Exception(message) { }
 public class ThreePlayersRequiredException(string message): Exception(message) { }
+public class InvalidAnswer(string message): Exception(message) { }
+public class InvalidVote(string message): Exception(message) { }
