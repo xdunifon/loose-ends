fb532137de83f1edfa3fb43918e1d9b7224c0487
xdunifon <xdunifon@gmail.com>
2026-02-25 21:08:27 -0500

    Add all signal r events

---
+++ LooseEndsClient/src/services/signalREvents.js
@@ -0,0 +1,14 @@
+export const events = {
+    playerJoined: 'PlayerJoined',
+    playerSubmitted: 'PlayerSubmitted',
+    playerVoted: 'PlayerVoted',
+
+    gameStarted: 'GameStarted',
+    gameOver: 'GameOver',
+
+    roundStarted: 'RoundStarted',
+    promptingEnded: 'PromptingEnded',
+    roundEnded: 'RoundEnded',
+    votingStarted: 'VotingStarted',
+    votingEnded: 'VotingEnded'
+}
