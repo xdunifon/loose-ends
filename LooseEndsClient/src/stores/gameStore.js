708b26127a289a6fd9a919a2f0fb5ae404efd909
xdunifon <xdunifon@gmail.com>
2026-02-28 09:15:40 -0500

    Setup bones of all signal r events

--- LooseEndsClient/src/stores/gameStore.js
+++ LooseEndsClient/src/stores/gameStore.js
@@ -43,11 +43,68 @@ export const useGameStore = defineStore('game', () => {
       }
     })
 
+    signalRService.on(events.gameOver, (dto) => {
+      console.log(events.gameOver, dto)
+
+      // Mark final round completed and end entire game
+      // Show final leaderboard + player winner
+    })
+
+    signalRService.on(events.roundStarted, (dto) => {
+      console.log(events.roundStarted, dto)
+
+      // get round using dto.roundNumber
+      // set answer due date on round
+    })
+
+    signalRService.on(events.promptingEnded, (dto) => {
+      console.log(events.promptingEnded, dto)
+
+      // mark prompting completed for current round
+      // Or stop somehow
+    })
+
+    signalRService.on(events.votingStarted, (dto) => {
+      console.log(events.votingStarted, dto)
+
+      // get current round and prompmt id
+      // sest vote due date
+      // present options to player
+    })
+
+    signalRService.on(events.votingEnded, (dto) => {
+      console.log(events.votingEnded, dto)
+
+      // mark prompt voting over
+      // stop voting and display results
+    })
+
+    signalRService.on(events.roundEnded, (dto) => {
+      console.log(events.roundEnded, dto)
+
+      // Mark round ended
+      // Display leaderboard results
+    })
+
     if (isHost.value) {
       signalRService.on(events.playerJoined, (dto) => {
         console.log(events.playerJoined, dto)
 
         players.value.push(dto)
+      })
+
+      signalRService.on(events.playerSubmitted, (playerId) => {
+        console.log(events.playerSubmitted, playerId)
+
+        // get active round, find prompt with plaerId, set submitted?
+        // If all players submitted, send out next()
+      })
+
+      signalRService.on(events.playerVoted, (playerId) => {
+        console.log(events.playerVoted, playerId)
+        
+        // get active voting prompt, find player with this id, set submitted?
+        // If all players submitted, send out next()
       })
     }
   }
